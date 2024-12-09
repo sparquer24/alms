b40c6dbd15e48696d8032b6e40026dc8830728d7
wmohammad <wraheman7864@gmail.com>
2024-12-09 15:29:15 +0530

    fe setup done

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const login = async (username: string, password: string) => {
+    const response = await axios.post("/api/login", { username, password });
+    return response.data;
+};
