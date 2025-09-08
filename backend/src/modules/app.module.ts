1d1252b54a0d2334f3371cd0eb1383316d79a966
sarvasuddhimanoj <manoj.sarvasuddhi@gmail.com>
2025-09-08 17:03:34 +0530

    crud apis,

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -5,8 +5,9 @@ import { AuthModule } from '../modules/auth/auth.module';
 import { ApplicationFormModule } from '../modules/FreshLicenseApplicationForm/application-form.module';
 import { LocationsModule } from '../modules/locations/locations.module';
 import { WorkflowModule } from '../modules/FLAWorkflow/workflow.module';
+import { StatusModule } from '../modules/status/status.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ApplicationFormModule, LocationsModule, WorkflowModule],
+  imports: [UserModule, StatusModule, AuthModule, ApplicationFormModule, LocationsModule, WorkflowModule],
 })
 export class AppModule {}
