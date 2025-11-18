c63078471cc158ea2b6f9c436ebdfce8060416ea
andrurandom <giribabunettlinx@gmail.com>
2025-11-18 13:19:35 +0530

    Set default baseURL for axios instance to localhost if environment variable is not defined

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jsCookie from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
