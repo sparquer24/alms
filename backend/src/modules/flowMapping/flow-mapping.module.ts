c407f83a1fcc76df576657dc432849defa18316b
andrurandom <giribabunettlinx@gmail.com>
2025-11-21 17:17:04 +0530

    refactor(flowMapping): remove PrismaService from providers

--- backend/src/modules/flowMapping/flow-mapping.module.ts
+++ backend/src/modules/flowMapping/flow-mapping.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FlowMappingController } from './flow-mapping.controller';
 import { FlowMappingService } from './flow-mapping.service';
-import { PrismaService } from '@/db/prisma.service';
 
 @Module({
     controllers: [FlowMappingController],
-    providers: [FlowMappingService, PrismaService],
+    providers: [FlowMappingService],
     exports: [FlowMappingService],
 })
 export class FlowMappingModule { }
